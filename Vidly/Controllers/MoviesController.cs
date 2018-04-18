7ae4fba501dd91e77cc9382160fbc5ea22152b3f
HSD\aanderson <aanderson@hollandspecial.com>
2018-04-18 16:45:20 -0400

    completed 2.14 step

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,24 +12,13 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-
-            // return View(movie);
-            return Content("Hello World!");
+            return View(movie);
         }
 
-        public ActionResult Edit(int id)
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}): range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
     }
 }
